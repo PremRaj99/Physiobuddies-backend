0f8e3becaedb5b3d5ac76c90910bffbfd6b08732
Prem Raj <premraj299341@gmail.com>
2026-07-28 15:31:22 +0530

    feat: configure Nginx proxy for API server and define whitelist of allowed CORS domains

--- src/core/security/allowed-domains.ts
+++ src/core/security/allowed-domains.ts
@@ -1,6 +1,8 @@
 export const allowedDomains = [
   'https://physiobuddies.in',
   'https://physiobuddies.com',
+  'https://pb.premraj.online',
+  'https://premraj.online',
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:4173',
